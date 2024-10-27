a8550930bf3a7159a0c43069cfb8aef6107dc38a
kz23nyc <khinezar.mh@gmail.com>
2024-10-27 00:09:56 -0400

    Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,44 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import RecipeDetails from "./pages/RecipeDetails.jsx";
-import RecipePage from "./pages/RecipePage.jsx";
+import RecipeList from "./pages/RecipeList.jsx";
+import { RecipeProvider} from './context/Recip'
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx"
-import "./App.css";
-
-function App() {
-  //State for recipes if needed, assumed to be fetched and stored here
-  const[recipes, setRecipes] = useState ([]);
-
-  //fetch all recipes when component first render
-  useEffect(() => {
-  // fetch all recipes
-  const fetchRecipes = async() => {
-    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/projects/`);
-  const recipesData = await res.json();
-  console.log(recipesData);
-  setRecipes(recipesData.projects);  
-  };
-
-  fetchRecipes();
-  }, [])
 
 
-  return (
+// function App() {
+//   //State for recipes if needed, assumed to be fetched and stored here
+//   const[recipes, setRecipes] = useState ([]);
+
+//   //fetch all recipes when component first render
+//   useEffect(() => {
+//   // fetch all recipes
+//   const fetchRecipes = async() => {
+//     const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/projects/`);
+//   const recipesData = await res.json();
+//   console.log(recipesData);
+//   setRecipes(recipesData.projects);  
+//   };
+
+//   fetchRecipes();
+//   }, [])
+
+
+ const App= () => (
+  <RecipeProvider>
     <Router>
     <NavBar />
     <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="/recipes" element={<RecipePage recipes={recipes} />} />
+    <Route path="/recipes" element={<RecipeList />} />
     <Route path="/recipes/:id" element={<RecipeDetails />} />
     <Route path="/login" element={<Login/>} />
     <Route path="/signup" element={<Signup/>} />
     </Routes> 
     </Router>   
-  )
-}
+    </RecipeProvider>
+  );
+
 
 export default App;
--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import { RecipeContext } from '../context/RecipeContext'; 
 
 const RecipePage = () => {
-  const { state } = useContext(RecipeContext);
-  const { recipes, loading, error } = state; // Pull out the recipes, loading, error properties from the state 
+  const { state } = useContext(RecipeContext); // access to the global state by RecipeContext in the app
+  const { recipes, loading, error } = state; // Pull out the recipes, loading, error properties from the state to use them in our component logic
 
   // Conditional rendering
   if (loading) return <p style={styles.message}>Loading...</p>; // Loading State
@@ -13,13 +13,15 @@ const RecipePage = () => {
   if (!recipes || recipes.length === 0) return <p style={styles.message}>No recipes found.</p>;
 
   // Rendering the recipe list
-  return ({recipes.map(recipe => (
+  return (
+    {recipes.map(recipe => (
+        <div>
         <div>
           <img
             src={recipe.image || 'https://via.placeholder.com/128x195.png?text=No+Image'}
             alt={recipe.title}
             style={styles.image}
-          />
+          /></div>
           <h3>{recipe.title}</h3>
           <Link to="/" >View Details</Link>       
         </div>
